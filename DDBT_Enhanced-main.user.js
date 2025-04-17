fd0374cded01bff060dab72f5d4e25450eca132b
diieus <diieus26@gmail.com>
2025-04-17 17:58:44 -0400

    Update DDBT_Enhanced-main.user.js
    MAJ épuisement

--- DDBT_Enhanced-main.user.js
+++ DDBT_Enhanced-main.user.js
@@ -70,18 +70,10 @@
 La créature a un désavantage aux jets d'attaque.
 Un jet d'attaque contre la créature a un avantage si l'attaquant est à 1,50 mètre ou moins de la créature. Sinon, le jet d'attaque a un désavantage.`,
 
-        "Épuisement": `Certaines capacités spéciales et dangers naturels, comme la famine et les effets d'une exposition prolongée au froid ou à la chaleur, peuvent conduire à un état spécial appelé l'épuisement. L'épuisement se mesure en six niveaux. Un effet peut donner à une créature un ou plusieurs niveaux d'épuisement, comme mentionné dans la description de l'effet.
-Niveau - Effet
-1 - Désavantage aux jets de caractéristique
-2 - Vitesse diminuée de moitié
-3 - Désavantage aux jets d'attaque et de sauvegarde
-4 - Maximum de points de vie diminué de moitié
-5 - Vitesse réduite à 0
-6 - Mort
-
+        "Épuisement": `Une créature épuisée subit un malus à tous ses d20 de -2 par niveau d'épuisement. De plus, sa vitese est diminuée de 1,50m par niveau d'épuisement.
+Certaines capacités spéciales et dangers naturels, comme la famine et les effets d'une exposition prolongée au froid ou à la chaleur, peuvent conduire à un état spécial appelé l'épuisement. L'épuisement se mesure en six niveaux. Un effet peut donner à une créature un ou plusieurs niveaux d'épuisement, comme mentionné dans la description de l'effet.
 Si une créature déjà épuisée subit un autre effet qui cause l'épuisement, son niveau actuel d'épuisement augmente par le nombre mentionné dans l'effet d'épuisement.
-Une créature subit les effets de son niveau d'épuisement plus ceux des niveaux inférieurs. Par exemple, une créature qui souffre d'un épuisement de niveau deux voit sa vitesse diminuée de moitié et a un désavantage aux jets de caractéristique.
-Un effet qui supprime l'épuisement réduit son niveau tel que mentionné dans la description de l'effet, et tous les effets reliés à l'épuisement disparaissent si le niveau d'épuisement d'une créature est réduit à moins de 1.
+Un effet qui supprime l'épuisement réduit son niveau tel que mentionné dans la description de l'effet.
 Terminer un repos long réduit le niveau d'épuisement d'une créature de 1, à condition que la créature ait aussi mangé et bu. De même, être rappelé à la vie réduit le niveau d'épuisement d'une créature de 1.`,
 
         "Agrippé": `La vitesse d'une créature agrippée passe à 0, et elle ne peut bénéficier d'aucun bonus à sa vitesse.
@@ -146,7 +138,8 @@ La créature est immunisée contre le poison et la maladie, mais un poison ou un
         <span style="display: inline-block; width: 30px;"></span>&#127919;<span style="color: red; font-weight: bold;">&#8594;Désavantage</span>
         `,
         "Épuisement": `
-        <span style="display: inline-block; width: 30px;"></span>&#127919;<span style="color: red; font-weight: bold;">&#8594;Désavantage (si nv 3+)</span>
+        <span style="display: inline-block; width: 30px;"></span>&#127922;<span style="color: red; font-weight: bold;"> -2 aux d20 / niveau d'épuisement</span><br>
+	<span style="display: inline-block; width: 30px;"></span> &#127939;<span style="color: red; font-weight: bold;"> -1,50m / niveau d"épuisement</span>
         `,
         "Agrippé": `
         <span style="display: inline-block; width: 30px;"></span><span style="font-weight: bold; text-decoration: line-through;">Vitesse</span> &#127939;
